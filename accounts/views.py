b4bb568285c120f14904ef825678df9fb4bc7df6
Phil Knowles <fighters4christ@outlook.com>
2018-02-28 16:09:11 -0600

    Login Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def home(request):
     numbers = [1, 2, 3, 4, 5]
     name = 'Phil Knowles'
@@ -25,12 +28,13 @@ def register(request):
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
 
-
+@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
 
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -44,6 +48,7 @@ def edit_profile(request):
         return render(request, 'accounts/edit_profile.html', args)
 
 
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
