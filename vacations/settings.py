189e70799529b0115bcaf53ada61d7cad37d98e0
Phil Knowles <fighters4christ@outlook.com>
2018-02-28 16:19:08 -0600

    Start of Middleware

--- vacations/settings.py
+++ vacations/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'vacations.middleware.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'vacations.urls'
