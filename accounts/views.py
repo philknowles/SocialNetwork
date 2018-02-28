c47db2828542c1bff7f607c5a90b638c7d7670bc
Phil Knowles <fighters4christ@outlook.com>
2018-02-28 14:07:27 -0600

    Added username information

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-
+from accounts.forms import RegistrationForm
 
 # Create your views here.
 def home(request):
@@ -15,12 +15,12 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/accounts')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
 
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
