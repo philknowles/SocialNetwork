c9b5bd0d60d63f491f8702bc961e380ffcf33383
Phil Knowles <fighters4christ@outlook.com>
2018-03-02 14:03:09 -0600

    set up form to post and retrieve data on the homepage

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    post = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete='CASCADE')
+    date = models.DateTimeField(auto_now=True)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from home.forms import HomeForm
-
+from home.models import Post
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -9,4 +9,20 @@ class HomeView(TemplateView):
 
     def get(self, request):
         form = HomeForm()
-        return render(request, self.template_name, {'form': form})
+        posts = Post.objects.all()
+
+        args = {'form': form, 'posts': posts}
+        return render(request, self.template_name, args)
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
+            text = form.cleaned_data['post']
+            form = HomeForm()
+
+            args = {'form': form, 'text': text}
+            return render(request, self.template_name, args)
