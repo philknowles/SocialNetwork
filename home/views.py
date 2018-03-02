a4bd85dd369dc509828069bc3de384c6761fca22
Phil Knowles <fighters4christ@outlook.com>
2018-03-02 14:49:40 -0600

    Fixing Home page redirect after post submission.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from home.forms import HomeForm
 from home.models import Post
 
@@ -24,6 +24,7 @@ class HomeView(TemplateView):
 
             text = form.cleaned_data['post']
             form = HomeForm()
+            return redirect('home:home')
 
             args = {'form': form, 'text': text}
             return render(request, self.template_name, args)
