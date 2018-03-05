e5702cde2767544e710159c4d0099c688c50bd32
Phil Knowles <fighters4christ@outlook.com>
2018-03-05 17:28:46 -0600

    Using Postgres

--- vacations/settings.py
+++ vacations/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'vacations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'hotspots',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'hotspots',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'family',
+        'USER': 'postgres',
+        'PASSWORD': 'M@lu6000',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
