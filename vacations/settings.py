3b30d5d91074543937e2e9e575b5aa9f70d08538
Phil Knowles <fighters4christ@outlook.com>
2018-03-16 10:49:21 -0500

    Fixed register button to redirect

--- vacations/settings.py
+++ vacations/settings.py
@@ -152,7 +152,7 @@ LOGIN_URL = '/accounts/login/'
 
 LOGIN_EXEMPT_URLS = (
     r'^accounts/logout/$',
-    r'^accounts/register%',
+    r'^accounts/register/$',
     r'^accounts/reset-password/$',
     r'^accounts/reset-password/done/$',
     r'^accounts/reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
