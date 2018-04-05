3b9deaa6a5034c1c9fd55372a847c3b341e2aa15
Phil Knowles <fighters4christ@outlook.com>
2018-04-05 16:35:26 -0500

    Added Tags and similar posts

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -47,9 +48,16 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',
+                                                                             '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
